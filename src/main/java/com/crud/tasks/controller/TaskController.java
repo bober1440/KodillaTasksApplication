9f98ba0acc6739270e75d369490fb78e446c05fc
bober1440 <mbobrowicz2@gmail.com>
2024-10-09 18:31:44 +0200

    added controller

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable  Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @DeleteMapping(value = "{taskId")
+    public void deleteTask(@PathVariable  Long taskId) {
+    }
+
+    @PutMapping
+    public TaskDto updateTask(@RequestBody  TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+    }
+}
